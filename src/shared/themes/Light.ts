59d236381d53b7923f919b098b29deb53380fb81
heronlancellot <heronlancellot@id.uff.br>
2023-08-22 15:10:35 -0300

    Feat: Add Theme Context

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material";
+import { cyan, yellow } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+    palette: {
+
+        primary: {                      // Cor de maior Destaque
+            main: yellow[700],
+            dark: yellow[700],
+            light: yellow[700],
+            contrastText: "#ffffff",    // Cor de constrate para a cor primária
+
+        },
+        secondary: {
+            main: cyan[500],
+            dark: cyan[400],
+            light: cyan[300],
+            contrastText: "#ffffff",    // Cor de constrate para a cor primária
+
+        },
+        background: {
+            default: "#ffffff" ,        // Cor que será utilizada no fundo
+            paper: "#f7f6f3",           // Cor que que destaca um pouco mais o fundo, exemplo card
+        }
+    }
+})
